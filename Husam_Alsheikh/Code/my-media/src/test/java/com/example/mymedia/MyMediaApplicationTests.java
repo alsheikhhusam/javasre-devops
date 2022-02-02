5b2eca603cc14fa1b00156fddde2eba3ac2bb7db
HusamAlsheikh <alsheikhhusam@gmail.com>
2022-02-02 14:17:57 -0500

    My Media Spring Boot Demo Start Project

---
+++ Husam_Alsheikh/Code/my-media/src/test/java/com/example/mymedia/MyMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
