d045fad3670b949bbac3d25a0c30d877e6a224e2
August Duet <august.duet@revature.com>
2022-02-17 10:13:14 -0500

    testing and helm

--- Week3_HTML_Spring/testing-demo/src/main/java/com/example/testingdemo/repos/ModelRepository.java
+++ Week3_HTML_Spring/testing-demo/src/main/java/com/example/testingdemo/repos/ModelRepository.java
@@ -2,8 +2,16 @@ package com.example.testingdemo.repos;
 
 import com.example.testingdemo.entities.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ModelRepository extends JpaRepository<Model, Long> {
+    List<Model> findAllByValue(String value);
+    List<Model> findAllByValueLike(String value);
+    @Query(nativeQuery = true, value = "select count(*) from models where value like :query")
+    Long countAllByValueLike(@Param("query") String query);
 }
