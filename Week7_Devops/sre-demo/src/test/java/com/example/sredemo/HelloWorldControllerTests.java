751d93c7deefd2e8d85b1633a017fb4246e37c31
August Duet <august.duet@revature.com>
2022-02-23 18:47:46 -0500

    adding ci/cd demo

---
+++ Week7_Devops/sre-demo/src/test/java/com/example/sredemo/HelloWorldControllerTests.java
@@ -0,0 +1,24 @@
+package com.example.sredemo;
+
+import com.example.sredemo.controllers.HelloWorldController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldReturnHelloWorld() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/hw"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=UTF-8"));
+    }
+}
---
+++ Week7_Devops/sre-demo/src/test/java/com/example/sredemo/SreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
